6f9392f120edb9b2f2f8e0c6cc0c5491e1bb6785
luphord <luphord@protonmail.com>
2020-09-22 18:32:46 +0200

    add black code formatter and reformat

--- mcc.py
+++ mcc.py
@@ -1,34 +1,32 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-'''Composable financial contracts with Monte Carlo valuation
-'''
+"""Composable financial contracts with Monte Carlo valuation
+"""
 
-__author__ = '''luphord'''
-__email__ = '''luphord@protonmail.com'''
-__version__ = '''0.1.0'''
+__author__ = """luphord"""
+__email__ = """luphord@protonmail.com"""
+__version__ = """0.1.0"""
 
 
 from argparse import ArgumentParser, Namespace
 
 parser = ArgumentParser(description=__doc__)
-parser.add_argument('--version',
-                    help='Print version number',
-                    default=False,
-                    action='store_true')
+parser.add_argument(
+    "--version", help="Print version number", default=False, action="store_true"
+)
 
-subparsers = parser.add_subparsers(title='subcommands', dest='subcommand',
-                                   help='Available subcommands')
+subparsers = parser.add_subparsers(
+    title="subcommands", dest="subcommand", help="Available subcommands"
+)
 
-mycmd_parser = subparsers.add_parser('mycmd', help='An example subcommand')
-mycmd_parser.add_argument('-n', '--number',
-                          help='some number',
-                          default=17, type=int)
+mycmd_parser = subparsers.add_parser("mycmd", help="An example subcommand")
+mycmd_parser.add_argument("-n", "--number", help="some number", default=17, type=int)
 
 
 def _mycmd(args: Namespace) -> None:
-    print('Running mycmd subcommand with n={}...'.format(args.number))
-    print('mycmd completed')
+    print("Running mycmd subcommand with n={}...".format(args.number))
+    print("mycmd completed")
 
 
 mycmd_parser.set_defaults(func=_mycmd)
@@ -37,10 +35,10 @@ mycmd_parser.set_defaults(func=_mycmd)
 def main() -> None:
     args = parser.parse_args()
     if args.version:
-        print('''Monte Carlo Contracts ''' + __version__)
-    elif hasattr(args, 'func'):
+        print("""Monte Carlo Contracts """ + __version__)
+    elif hasattr(args, "func"):
         args.func(args)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- tests/test_mcc.py
+++ tests/test_mcc.py
@@ -4,9 +4,8 @@ from mcc import parser
 
 
 class TestMonteCarloContracts(unittest.TestCase):
-
     def test_argument_parsing(self):
         args = parser.parse_args([])
         self.assertEqual(args.version, False)
-        args = parser.parse_args(['--version'])
+        args = parser.parse_args(["--version"])
         self.assertEqual(args.version, True)
