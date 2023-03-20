725f8263638864a0e0f7a2ef00ad22469b48be6d
luphord <luphord@protonmail.com>
2023-03-20 19:32:10 +0100

    Bump version: 0.7.0 → 0.8.0

--- mcc.py
+++ mcc.py
@@ -6,7 +6,7 @@
 
 __author__ = """luphord"""
 __email__ = """luphord@protonmail.com"""
-__version__ = """0.7.0"""
+__version__ = """0.8.0"""
 
 
 from argparse import ArgumentParser, Namespace
--- setup.py
+++ setup.py
@@ -49,6 +49,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/luphord/monte-carlo-contracts",
-    version="0.7.0",
+    version="0.8.0",
     zip_safe=True,
 )
