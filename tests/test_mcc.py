d69d88e01cc55fdb7baa06955fbdf39f82d01933
luphord <luphord@protonmail.com>
2023-03-25 21:26:14 +0100

    extract test module for stochastic processes

--- tests/test_mcc.py
+++ tests/test_mcc.py
@@ -30,10 +30,6 @@ from mcc import (
     Anytime,
 )
 from mcc.pricing_models import simulate_equity_black_scholes_model
-from mcc.pricing_models.stochastic_processes import (
-    BrownianMotion,
-    GeometricBrownianMotion,
-)
 
 from .test_utils import make_model, AlternatingBool
 
@@ -749,35 +745,3 @@ class TestMonteCarloContracts(unittest.TestCase):
         c = When(At(model.dategrid[-1]), One("EUR"))
         npv = evaluate(model, c)
         self.assertEqual(npv, 1)
-
-    def test_brownian_motions(self) -> None:
-        mu = 0.123
-        sigma = 0.456
-        bm = BrownianMotion(mu_t=lambda t: mu * t, sigma=sigma)
-        gbm = GeometricBrownianMotion(mu_t=lambda t: mu * t, sigma=sigma)
-        rnd = np.random.RandomState(1234)
-        t = np.linspace(0, 20, 20)
-        n = 200
-        x = bm.simulate(t, n, rnd)
-        bm.expected(t), bm.stddev(t)
-        bm.moment_match(t, x)
-        self.assertEqual((n, t.size), x.shape)
-        self.assertEqual(n, x[:, -1].size)
-        x = gbm.simulate(t, n, rnd)
-        self.assertEqual((n, t.size), x.shape)
-        self.assertEqual(n, x[:, -1].size)
-        gbm.expected(t), gbm.stddev(t)
-        gbm.moment_match(t, x)
-
-    def test_moment_matched_brownian_motions(self) -> None:
-        bm = BrownianMotion(lambda t: t)
-        rnd = np.random.RandomState(1234)
-        t = np.linspace(0, 20, 20)
-        n = 200
-        x = bm.simulate_with_moment_matching(t, n, rnd)
-        self.assertTrue(np.allclose(x.mean(axis=0), t))
-        self.assertTrue(np.allclose(x.std(axis=0), np.sqrt(t)))
-        gbm = GeometricBrownianMotion()
-        x = gbm.simulate_with_moment_matching(t, n, rnd)
-        self.assertTrue(np.allclose(x.mean(axis=0), np.ones(t.shape)))
-        self.assertTrue(np.allclose(x.var(axis=0), np.exp(t) - 1))
